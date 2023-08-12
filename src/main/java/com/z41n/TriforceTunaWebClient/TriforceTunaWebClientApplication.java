28d4dec8816a2c348a8fda06396f6eea5d91c6ff
Zain <zain.sheraz@gmail.com>
2023-08-12 11:36:56 -0400

    Initial commit

---
+++ src/main/java/com/z41n/TriforceTunaWebClient/TriforceTunaWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.z41n.TriforceTunaWebClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriforceTunaWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriforceTunaWebClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/z41n/TriforceTunaWebClient/TriforceTunaWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z41n.TriforceTunaWebClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriforceTunaWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
