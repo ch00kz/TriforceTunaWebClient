c9ee62b70cac61d3a27bd5c1827cf197ed098788
Zain <zain.sheraz@gmail.com>
2023-08-12 23:43:05 -0400

    Delete AppService.js

--- frontend/src/services/AppService.js
+++
@@ -1,9 +0,0 @@
-export async function getSummonerInfo(summonerName) {
-    const response = await fetch(`/api/summoner-info?summonerName=${summonerName}`);
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
-}
-
-
